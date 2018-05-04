e9ea4477383db0d79baa4ae0858adfa7749523c7
ostappartyka <ostap.partyka@cloudtp.com>
2018-05-04 13:45:33 +0300

    Deploy

---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '862106608121-ucdvb26er0vln5o8u6esg13ckpe7uk8k.apps.googleusercontent.com',
+  googleClientSecret: 'sHj82Hv71vNjY41Yl1YKCVKB',
+  mongoURI: 'mongodb://admin:admin@ds255309.mlab.com:55309/node-js-test-app-dev',
+  cookieKey: 'someuselesscookietest'
+}
