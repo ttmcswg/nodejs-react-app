b9e40d5990d89138da085854b3116d50e2e64598
ostappartyka <ostap.partyka@cloudtp.com>
2018-05-17 15:35:55 +0300

    Fixed bug with credits after update.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,7 +16,7 @@ class Header extends Component{
       default:
         return [
           <li key="1"><Payments /></li>,
-          <li key="2">
+          <li key="2" style={{ margin: '0 10px'}}>
             Credits: {this.props.auth.credits}
           </li>,
           <li key="3"><a href="/api/logout">Logout</a></li>
--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -13,7 +13,6 @@ module.exports = app => {
 
     req.user.credits += 5;
     const user = await req.user.save();
-
     res.send(user);
   });
 };
